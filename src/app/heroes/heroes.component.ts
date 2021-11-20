68dd7e8982e5a2e043c7f4f12e6617dacf68f97d
walter-cf <walter-cf@nepextel..hu>
2021-11-20 12:53:45 +0100

    Add navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,7 +11,6 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
 
-  selectedHero?: Hero;
 
   heroes: Hero[] = [];
 
@@ -21,10 +20,13 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
+  /*
+  selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
+  */
 
   getHeroes(): void {
     this.heroService.getHeroes()
